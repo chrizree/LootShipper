ee917eca3397aa3864b1288620b0c0a14914c5e0
chrizree <67806507+chrizree@users.noreply.github.com>
2020-07-12 22:20:47 +0200

    Update lootshipper_rpi.sh

--- Packet Squirrel/lootshipper_rpi.sh
+++ Packet Squirrel/lootshipper_rpi.sh
@@ -20,8 +20,8 @@
 #
 # This script-set is based on the scenario that the Raspberry Pi collects loot and
 # produces a text based loot file. This loot file is transfered to the chosen
-# Hak5 device (in this case a LAN Turtle "Gen 1" which is physically attached to
-# one of the USB ports of the Raspberry Pi) using scp. The scripts are not bullet
+# Hak5 device (in this case a Packet Squirrel in arming mode which is physically
+# attached to the ethernet port of the Raspberry Pi) using scp. The scripts are not bullet
 # proof and are for sure in need of certain improval. But, it has been created
 # with the following in mind; "Don't let perfect get in the way of good enough".
 #
