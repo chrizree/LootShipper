867139f0007f9c116f46459c3a1450aab1daa336
chrizree <67806507+chrizree@users.noreply.github.com>
2020-07-13 16:23:09 +0200

    Update loot-gen.sh

--- LAN Turtle/loot-gen.sh
+++ LAN Turtle/loot-gen.sh
@@ -2,7 +2,7 @@
 
 # Temporary bash script to generate "fake" loot for testing purposes
 
-# Add as cron job on Raspberry Pi
+# Add as cron job on the Raspberry Pi
 # 10 * * * * /home/pi/loot-gen.sh >/dev/null 2>&1
 
 DATENOW=$(/bin/date '+%Y-%m-%d %H:%M:%S')
